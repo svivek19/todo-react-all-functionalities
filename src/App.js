ab9adc696ef9e804107ab4985ee7d72b3ef8a522
svivek19 <svivek030503@gmail.com>
2023-10-28 20:25:24 +0530

    task completed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
 import React, { useState } from 'react'
+import ListItems from './ListItems';
 
 const App = () => {
 
   const [name, setname] = useState();
   const [input, setinput] = useState([]);
 
-  const handleAdd = (e) => {
-    setname(e.target.value);
+  const handleInput = (e) => {
+    setname(e.target.value)
+  }
+  const handleAdd = () => {
+    setinput([...input, { text: name, completed: false }])
+    setname("")
   }
 
-  const handleInput = () => {
+  const handleToggle = (index) => {
+    const updatedVal = [...input];
+    updatedVal[index].completed = !updatedVal[index].completed
+    setinput(updatedVal)
+  }
 
+  const handleDel = (index) => {
+    const updatedVal = [...input];
+    updatedVal.splice(index, 1)
+    setinput(updatedVal)
   }
 
   return (
     <div>
       <input type='text' placeholder='enter your task' value={name} onChange={handleInput} />
       <button onClick={handleAdd}>ADD</button>
+      {input.map((items, index) => (
+        <ListItems
+          key={items}
+          text={items.text}
+          completed={items.completed}
+          handleToggle={() => handleToggle(index)}
+          handleDel={() => handleDel(index)}
+        />
+      ))}
     </div>
   )
 }
--- src/ListItems.js
+++ src/ListItems.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-export default function ListItems() {
-  return (
-    <div>
-      
-    </div>
-  )
+export default function ListItems(props) {
+    const { text, handleToggle, handleDel, completed } = props;
+    return (
+        <div>
+            <h3 style={{color: completed? "green":"red"}}>{text}</h3>
+            <button onClick={handleToggle}>{completed ? "undo" : "Finish"}</button>
+            <button onClick={handleDel}>Delete</button>
+        </div>
+    )
 }
