9aa25b5525ed6af4918165e83b16dc47388375f6
svivek19 <svivek030503@gmail.com>
2023-10-28 19:44:06 +0530

    complete the UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  const [name, setname] = useState();
+  const [input, setinput] = useState([]);
+
+  const handleAdd = (e) => {
+    setname(e.target.value);
+  }
+
+  const handleInput = () => {
+
+  }
+
   return (
     <div>
-      
+      <input type='text' placeholder='enter your task' value={name} onChange={handleInput} />
+      <button onClick={handleAdd}>ADD</button>
     </div>
   )
 }
---
+++ src/ListItems.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ListItems() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
